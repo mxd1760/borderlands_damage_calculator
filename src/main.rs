e91043df6a28a4b9aae620afd3492fc21712df10
Marcus Doucette <Marz3587@gmail.com>
2024-06-20 22:30:54 -0400

    file saving (loading crashes)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use eframe::egui::{containers, widgets, Widget};
+use std::{any, path::Path};
 
+use eframe::egui::{containers, widgets, Widget};
+use serde::{Deserialize, Serialize};
 
 #[derive(PartialEq)]
 enum DamageCalculationMethod{
@@ -29,9 +31,10 @@ enum View{
   Default,
   EnterGun,
   ShowDPS,
+  SavedGunList,
 }
 
-#[derive(PartialEq)]
+#[derive(Serialize,Deserialize,PartialEq, Clone, Debug)]
 enum GunType{
   Pistol,
   Shotgun,
@@ -65,12 +68,12 @@ enum Game{
   Borderlands3,
   TinyTinasWonderland,
 }
-
+#[derive(Serialize,Deserialize,Clone, Debug)]
 struct ElementalInfo{
   //TODO
 }
 
-
+#[derive(Serialize,Deserialize,Clone, Debug)]
 struct Gun{
   name:String,
   gun_type:GunType,
@@ -85,6 +88,43 @@ struct Gun{
 
 impl Gun{
 }
+
+// impl Serialize for Gun{
+//   fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//       where
+//           S: serde::Serializer {
+//       serializer.serialize_bytes(self.name.as_bytes());
+//       self.gun_type.serialize(serializer);
+//       serializer.serialize_i32(self.damage);
+//       serializer.serialize_f64(self.accuracy);
+//       serializer.serialize_f64(self.fire_rate);
+//       serializer.serialize_f64(self.reload_speed);
+//       serializer.serialize_i16(self.magazine_size);
+//       self.elemental_info.serialize(serializer);
+//       self.other.serialize(serializer)
+//   }
+// }
+
+// struct ForeVisitor;
+
+// impl<'de> serde::de::Visitor<'de> for ForeVisitor{
+//   type Value = any;
+//   fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+//       formatter.write_str("i shouldn't need to write 100 different visitors if serde is doing the encoding")
+//   }
+//   fn visit_bytes<E>(self,value:&[u8]) -> Result<self
+// }
+
+// impl Deserialize<'de> for Gun{
+//   fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+//       where
+//           D: serde::Deserializer<'de> {
+//     Self{
+//       name:String.from(deserializer.deserialize_bytes(ForeVisitor)?);
+//     }
+//   }
+// }
+
 
 impl Default for Gun{
   fn default() -> Self {
@@ -105,7 +145,8 @@ struct MyApp{
   current_view:View,
   current_gun:Gun,
   current_game:Game,
-  damage_calculation_method:DamageCalculationMethod
+  damage_calculation_method:DamageCalculationMethod,
+  gun_list:Vec<Gun>
 }
 
 impl MyApp{
@@ -113,6 +154,51 @@ impl MyApp{
 }
 
 // Helper Functions
+fn load_gunlist() -> Vec<Gun>{
+  println!("Wow");
+  if let Some(gun_file) = get_gunfile(){
+    if let Ok(file_obj) = std::fs::File::open(gun_file){
+      // let mut data = Vec::new();
+      // file.read_to_end(&mut data).expect("unable to read data");
+      let decoded:Option<Vec<Gun>> = bincode::deserialize_from(file_obj).expect("unable to read data");
+      match decoded{
+        Some(guns) => {
+          println!("{:?}", guns);
+          return guns;
+        },
+        None => {println!("No Guns???!!!")}
+      }
+    }else{
+      println!("file not open")
+    }
+  }else{
+    println!("file path not found")
+  }
+  vec![]
+}
+
+fn get_gunfile() -> Option<std::path::PathBuf>{
+  if let Some(mut load_dir) = dirs::data_dir(){
+    load_dir.push("MDM");
+    load_dir.push("Borderlands Damage Calculator");
+    load_dir.push("guns_file.gunfi");
+    println!("{:?}",load_dir);
+    Some(load_dir)
+  }else{
+    None
+  }
+}
+
+fn save_gunlist(guns:&Vec<Gun>) {
+  if let Some(gun_file) = get_gunfile(){
+    std::fs::create_dir_all(gun_file.parent().unwrap()).unwrap();
+    if let Ok(file_obj) = std::fs::File::create(gun_file){
+      bincode::serialize_into(file_obj, guns).expect("Failed to write");
+    }
+    
+  }
+}
+
 fn pair(ui:&mut eframe::egui::Ui,label:&str,widget:impl Widget) -> eframe::egui::InnerResponse<()>{
   ui.horizontal(|ui|{
     ui.label(label);
@@ -127,6 +213,7 @@ impl Default for MyApp{
       current_gun:Gun::default(),
       current_game:Game::BorderlandsPreSequel,
       damage_calculation_method: DamageCalculationMethod::DamageXFireRate,
+      gun_list: load_gunlist(),
     }
   }
 }
@@ -145,6 +232,9 @@ impl eframe::App for MyApp{
         }
         if ui.button("DPS Calculator").clicked(){
           self.current_view = View::ShowDPS;
+        }
+        if ui.button("Gun List").clicked(){
+          self.current_view = View::SavedGunList;
         }
       });
       match self.current_view{
@@ -178,6 +268,13 @@ impl eframe::App for MyApp{
           pair(ui,"Magazine Size",widgets::DragValue::new(&mut gun.magazine_size));
           ui.label("TODO ELEMENTAL");
           ui.label("TODO BONUS STATS");
+          
+          if ui.button("Save data").clicked(){
+            let gun = self.current_gun.clone();
+            self.gun_list.push(gun);
+            save_gunlist(&self.gun_list)
+            // println!("{:?}", self.gun_list);
+          }
         },
         View::ShowDPS => {
           ui.horizontal(|ui|{
@@ -187,6 +284,15 @@ impl eframe::App for MyApp{
           });
           ui.label(format!("Your DPS is {:.2}",self.damage_calculation_method.calc_damage(&self.current_gun)));
         },
+        View::SavedGunList =>{
+          for i in &self.gun_list{
+            let copy = i.clone();
+            if ui.button(copy.name).clicked(){
+              let copy2 = i.clone();
+              self.current_gun = copy2;
+            }
+          }
+        }
       }
       
     });
